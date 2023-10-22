66f758090f36de4f1bc372860ec1feb055265195
Hisham Sally <41293102+hishamysally@users.noreply.github.com>
2023-10-22 15:08:00 +0530

    code to client and server added

--- client.py
+++ client.py
@@ -1,1 +1,19 @@
 # the client end
+
+import socket
+
+# Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect(("server_ip", 12345))
+
+# Send and receive messages
+while True:
+    message = input("You: ")
+    client_socket.send(message.encode())
+    response = client_socket.recv(1024).decode()
+    print(f"Server: {response}")
+
+# Close the connection
+client_socket.close()
--- server.py
+++ server.py
@@ -1,1 +1,28 @@
 # this is the server side
+
+import socket
+
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a specific IP and port
+server_socket.bind(("your_server_ip", 12345))
+
+# Listen for incoming connections
+server_socket.listen()
+
+print("Server is listening...")
+
+# Accept a connection from a client
+client_socket, client_address = server_socket.accept()
+print(f"Connected to {client_address}")
+
+# Receive and send messages
+while True:
+    message = client_socket.recv(1024).decode()
+    print(f"Client: {message}")
+    response = input("You: ")
+    client_socket.send(response.encode())
+
+# Close the connection
+client_socket.close()
